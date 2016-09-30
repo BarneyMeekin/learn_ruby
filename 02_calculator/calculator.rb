e4a63a5dc94bf6e5e65c72461a01cdf9e0ab61f3
Barney Meekin <BarneyJMeekin@gmail.com>
2016-10-01 09:46:45 +1300

    finished 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+def add (a,b)
+    a + b
+end
+
+def subtract (a,b)
+    a - b
+end
+
+def sum(array)
+    array.inject(0){ |total, num| total + num}
+end
+
+def multiply(a, b)
+    a * b
+end
+
+def power(a, b)
+    a ** b
+end
+
+def factorial(number)
+    if number <= 1
+        1
+     else
+        (1..number).inject(1, :*)
+    end
+end
