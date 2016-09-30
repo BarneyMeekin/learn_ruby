4eb20cb54d36858ef4797063adfa2e1ebe42f1b3
Barney Meekin <BarneyJMeekin@gmail.com>
2016-10-01 10:30:55 +1300

    finished 03 simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo(input)
+    input
+end
+
+def shout(input)
+    input.upcase
+end
+
+def repeat(input, num = 2)
+    ([input] * num).join " "
+end
+
+def start_of_word(input, num)
+    input[0...num]
+end
+
+def first_word(input)
+    input.split(" ").first
+end
+
+def titleize(input)
+    words = input.split.map do |word|
+        if %w(the and over is an it).include?(word)
+            word
+        else
+            word.capitalize
+        end
+    end
+    words.first.capitalize!
+    words.join(" ")
+end
