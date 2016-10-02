c2bb0c0056ca37c42d929199c54ae76adfc69de5
Barney Meekin <BarneyJMeekin@gmail.com>
2016-10-03 12:49:28 +1300

    completed book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+   attr_reader :title
+   def title=(name)
+      name = name.split(" ")
+      name[0].capitalize!
+      name.each do |word|
+         little_words = ['a','an','the','at','by','for','in','of','on','to','up',
+   		'and','as','but','it','or','nor','over']
+         if little_words.include? word
+            word
+         else
+            word.capitalize!
+         end
+      end
+      @title = name.join(" ")
+    end
 end
