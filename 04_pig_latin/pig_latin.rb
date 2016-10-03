7483ac0163f7bd7f5a68afb222ac2e6ee41c3d18
Barney Meekin <BarneyJMeekin@gmail.com>
2016-10-03 17:16:38 +1300

    finished pig_latin but not too please with it

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
-#write your code here
+def translate(input)
+    words = input.downcase.split(' ')
+    words = words.map do |word|
+        if word.start_with?('a', 'e', 'i', 'o', 'u')
+            word << "ay"
+        elsif word.start_with?('ch', 'qu', 'br', 'the')
+            word.delete(word[0..1]) << word[0..1] << "ay"
+        elsif word.start_with?('thr', 'sch', 'squ')
+            word.delete(word[0..2]) << word[0..2] << "ay"
+        else
+            word.delete(word[0]) << word[0] << "ay"
+        end
+    end
+    words.join (' ')
+end
